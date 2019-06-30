1949d72359ddc063385553bb8699d01ebf423a13
Christy Grimaud <christy.grimaud@gmail.com>
2019-06-30 17:12:59 -0400

    Add project files.

---
+++ BookClubTracker/MeetUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace BookClubTracker
+{
+
+    public class RootObject
+    {
+        public MeetUp[] MeetUp { get; set; }
+    }
+
+    public class MeetUp
+    {
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+        [JsonProperty(PropertyName = "author")]
+        public string Author { get; set; }
+        [JsonProperty(PropertyName = "genre")]
+        public string Genre { get; set; }
+        [JsonProperty(PropertyName = "meetUpDate")]
+        public string MeetUpDate { get; set; }
+        [JsonProperty(PropertyName = "meetUpLocation")]
+        public string MeetUpLocation { get; set; }
+    }
+
+}
+
---
+++ BookClubTracker/README.md
@@ -0,0 +1,5 @@
+﻿Book Club Tracker Console App
+
+In this project a user will be able to view all book club meet up details, edit book club meet up details, or add a new book club meet up.
+
+A book club meet up consists of book title, author, genre, date we met, and place we met. 
